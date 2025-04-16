85ad945f035ff91154479a871cc9b8590a4b364c
tonychg <tonychg7@pm.me>
2025-04-16 23:00:04 +0200

    fix(nvim-k8s-lsp): failed import

--- lua/nvim-k8s-lsp/init.lua
+++ lua/nvim-k8s-lsp/init.lua
@@ -1,10 +1,13 @@
 local M = {}
+local config
 
 local store_builtins_url =
-  "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/refs/heads/master/v1.32.2-standalone-strict/%s-%s-%s.json"
+  "https://raw.githubusercontent.com/yannh/kubernetes-json-schema/refs/heads/master/%s-standalone-strict/%s-%s-%s.json"
 local store_crds_url = "https://raw.githubusercontent.com/datreeio/CRDs-catalog/refs/heads/main/%s/%s_%s.json"
 
-local defaults = {}
+local defaults = {
+  kubernetes_version = "v1.32.2",
+}
 
 local function table_contains(tbl, x)
   local found = false
@@ -31,11 +34,11 @@ local function extract_kind(line)
 end
 
 local function is_builtin(attributes)
-  local builtins = require("kubernetes-lsp.builtins")
+  local builtins = require("nvim-k8s-lsp.builtins")
   return table_contains(builtins, attributes.kind)
 end
 
-local function get_schema_url(attributes)
+local function get_schema_url(kubernetes_version, attributes)
   local schema_url
   local kind = string.lower(attributes.kind)
   local group = attributes.group
@@ -43,7 +46,7 @@ local function get_schema_url(attributes)
   local base_url = store_builtins_url
 
   if is_builtin(attributes) then
-    schema_url = string.format(base_url, kind, group, version)
+    schema_url = string.format(base_url, kubernetes_version, kind, group, version)
   else
     base_url = store_crds_url
     schema_url = string.format(base_url, group, kind, version)
@@ -105,7 +108,7 @@ function M:associate_schema_to_buffer(client, bufnr)
   local attributes = M:extract_api_attributes(bufnr)
 
   if attributes.kind and attributes.version and attributes.group then
-    local schema_url = get_schema_url(attributes)
+    local schema_url = get_schema_url(config.kubernetes_version, attributes)
     local settings = client.settings
     local bufuri = vim.uri_from_bufnr(bufnr)
     local schemas = build_schemas(settings.yaml.schemas, schema_url, bufuri)
@@ -114,11 +117,11 @@ function M:associate_schema_to_buffer(client, bufnr)
   end
 end
 
-function M:setup(config)
-  local settings = defaults
+function M:setup(opts)
+  config = defaults
 
-  if config then
-    settings = vim.tbl_deep_extend("force", settings, config)
+  if opts then
+    config = vim.tbl_deep_extend("force", opts, config)
   end
 
   vim.api.nvim_create_autocmd("LspAttach", {
